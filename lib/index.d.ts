f9ea54c849289e55d784eda888a0cee569721fd3
Salvatore <dested@gmail.com>
2020-11-16 20:03:32 -0700

    cleaned lib folder

--- lib/index.d.ts
+++
@@ -1,3 +0,0 @@
-export * from './schemaDefiner';
-export * from './schemaDefinerTypes';
-//# sourceMappingURL=index.d.ts.map
--- lib/utils.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function objectSafeKeys<T>(obj: T): (keyof T)[];
-export declare function assert(assertion: boolean): asserts assertion;
-export declare function assertType<T>(assertion: any): asserts assertion is T;
-export declare function safeKeysExclude<T, TExclude extends keyof T>(obj: T, exclude: TExclude): Exclude<keyof T, TExclude>[];
-export declare function switchType<TType extends string | number, TResult>(n: TType, options: {
-    [key in TType]: TResult;
-}): TResult;
-//# sourceMappingURL=utils.d.ts.map
